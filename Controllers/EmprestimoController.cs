f2e49c42d3b76cc7278a3db8a90e6d31a16d0e3b
Marcelo Santos <m.santos1964@uol.com.br>
2022-04-10 16:35:24 -0300

    Correção na Autenticacao.cs e EmprestimoController.cs

--- Controllers/EmprestimoController.cs
+++ Controllers/EmprestimoController.cs
@@ -11,11 +11,12 @@ namespace Biblioteca.Controllers
     {
         public IActionResult Cadastro()
         {
+            Autenticacao.CheckLogin(this);
             LivroService livroService = new LivroService();
             EmprestimoService emprestimoService = new EmprestimoService();
 
             CadEmprestimoViewModel cadModel = new CadEmprestimoViewModel();
-            cadModel.Livros = livroService.ListarTodos();
+            cadModel.Livros = livroService.ListarDisponiveis();
             return View(cadModel);
         }
 
@@ -23,33 +24,39 @@ namespace Biblioteca.Controllers
         public IActionResult Cadastro(CadEmprestimoViewModel viewModel)
         {
             EmprestimoService emprestimoService = new EmprestimoService();
-            
+
             if(viewModel.Emprestimo.Id == 0)
-            {
+            {               
                 emprestimoService.Inserir(viewModel.Emprestimo);
             }
             else
             {
                 emprestimoService.Atualizar(viewModel.Emprestimo);
             }
-            return RedirectToAction("Listagem");
+            return RedirectToAction("Listagem");     
         }
 
-        public IActionResult Listagem(string tipoFiltro, string filtro)
+        public IActionResult Listagem(string tipoFiltro, string filtro, string itensPorPagina, int NumDaPagina, int PaginaAtual)
         {
+            Autenticacao.CheckLogin(this);
             FiltrosEmprestimos objFiltro = null;
-            if(!string.IsNullOrEmpty(filtro))
-            {
-                objFiltro = new FiltrosEmprestimos();
-                objFiltro.Filtro = filtro;
-                objFiltro.TipoFiltro = tipoFiltro;
-            }
+            if (!string.IsNullOrEmpty(filtro))
+                {
+                    objFiltro = new FiltrosEmprestimos();
+                    objFiltro.Filtro = filtro;
+                    objFiltro.TipoFiltro = tipoFiltro;
+                }
+
+            ViewData["emprestimosPorPagina"] = (string.IsNullOrEmpty(itensPorPagina) ? 10 : Int32.Parse(itensPorPagina));
+            ViewData ["PaginaAtual"] = (PaginaAtual != 0 ? PaginaAtual : 1);
+
             EmprestimoService emprestimoService = new EmprestimoService();
             return View(emprestimoService.ListarTodos(objFiltro));
         }
 
         public IActionResult Edicao(int id)
         {
+            Autenticacao.CheckLogin(this);
             LivroService livroService = new LivroService();
             EmprestimoService em = new EmprestimoService();
             Emprestimo e = em.ObterPorId(id);
@@ -57,7 +64,6 @@ namespace Biblioteca.Controllers
             CadEmprestimoViewModel cadModel = new CadEmprestimoViewModel();
             cadModel.Livros = livroService.ListarTodos();
             cadModel.Emprestimo = e;
-            
             return View(cadModel);
         }
     }
