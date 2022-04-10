6beb2994c898198a63a63bd31f380daea3ca7c47
Marcelo Santos <m.santos1964@uol.com.br>
2022-04-10 16:20:08 -0300

    Impletação de Usuarios.Controller

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,96 @@
+using Biblioteca.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+
+namespace Biblioteca.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public IActionResult ListaDeUsuarios()
+        {
+            Autenticacao.CheckLogin(this);
+            Autenticacao.verificaSeUsuarioEAdmin(this);
+
+            return View(new UsuarioService().Listar());
+        }
+
+        public IActionResult editarUsuario(int id)
+        {
+            Usuario u = new UsuarioService().Listar(id);
+
+            return View(u);
+        }
+
+        [HttpPost]
+        public IActionResult editarUsuario(Usuario userEditado)
+        {
+            UsuarioService us = new UsuarioService();
+            us.editarUsuario(userEditado);
+            return RedirectToAction("ListadeUsuarios");
+        }
+
+        public IActionResult RegistroDeUsuarios()
+        {
+            Autenticacao.CheckLogin(this);
+            Autenticacao.verificaSeUsuarioEAdmin(this);
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RegistroDeUsuarios(Usuario novoUser)
+        {
+            Autenticacao.CheckLogin(this);
+            Autenticacao.verificaSeUsuarioEAdmin(this);
+
+            novoUser.senha = Criptografo.TextoCriptografado(novoUser.senha);
+
+            UsuarioService us = new UsuarioService();
+            us.IncluirUsuario(novoUser);
+
+            return RedirectToAction("CadastroRealizado");
+        }
+
+        public IActionResult ExcluirUsuario(int id)
+        {
+            return View(new UsuarioService().Listar(id));
+        }
+
+        [HttpPost]
+        public IActionResult ExcluirUsuario(string decisao,int id)
+        {
+            if(decisao=="EXCLUIR")
+            {
+                ViewData["Mensagem"] = "Exclusao do usuário " +new UsuarioService().Listar(id).Nome+" realizada com sucesso";
+                new UsuarioService().excluirUsuario(id);
+                return View("ListaDeUsuarios", new UsuarioService().Listar());
+            }
+            else
+            {
+                ViewData["Mensagem"] = "Exclusão cancelada.";
+                return View("ListaDeUsuarios", new UsuarioService().Listar());
+            }
+        }
+
+        public IActionResult cadastroRealizado()
+        {
+            Autenticacao.CheckLogin(this);
+            Autenticacao.verificaSeUsuarioEAdmin(this);
+
+            return View();
+        }
+
+        public IActionResult NeedAdmin()
+        {
+            Autenticacao.CheckLogin(this);
+            return View();
+        }
+
+        public IActionResult Sair()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
